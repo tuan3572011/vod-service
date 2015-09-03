3d5d5395839c6a3e5a654a276e7c81fcc6837861
Nguyen Van Tuan <tuanvannguyen@kms-technology.com>
2015-09-03 11:19:18 +0700

    filter

--- src/main/java/com/vod/model/FilterBean.java
+++ src/main/java/com/vod/model/FilterBean.java
@@ -9,81 +9,75 @@ package com.vod.model;
  */
 public class FilterBean {
 
-	private int orderBy;
-	private Category category;
-	private int year;
-	private int countryId;
-	private int page;
+    private int orderBy;
+    private Category category;
+    private int year;
+    private Country country;
+    private int page;
 
-	public FilterBean(int orderBy, Category categoryId, int year, int countryId, int page) {
-		this.orderBy = orderBy;
-		this.category = categoryId;
-		this.countryId = countryId;
-		this.year = year;
-		this.page = page;
-	}
+    public FilterBean(int orderBy, Category categoryId, int year,
+            Country country, int page) {
+        this.orderBy = orderBy;
+        this.category = categoryId;
+        this.country = country;
+        this.year = year;
+        this.page = page;
+    }
 
-	public int getOrderBy() {
-		return orderBy;
-	}
+    public int getOrderBy() {
+        return orderBy;
+    }
 
-	public void setOrderBy(int orderBy) {
-		this.orderBy = orderBy;
-	}
+    public void setOrderBy(int orderBy) {
+        this.orderBy = orderBy;
+    }
 
-	public int getYear() {
-		return year;
-	}
+    public int getYear() {
+        return year;
+    }
 
-	public void setYear(int year) {
-		this.year = year;
-	}
+    public void setYear(int year) {
+        this.year = year;
+    }
 
-	public int getCountryId() {
-		return countryId;
-	}
+    public int getPage() {
+        return page;
+    }
 
-	public void setCountryId(int countryId) {
-		this.countryId = countryId;
-	}
+    public void setPage(int page) {
+        this.page = page;
+    }
 
-	public int getPage() {
-		return page;
-	}
+    public boolean isSearchAny() {
+        return this.isSearchByCategory() || isSearchByCountry()
+                || isSearchByYear();
+    }
 
-	public void setPage(int page) {
-		this.page = page;
-	}
+    public boolean isSearchByCategory() {
+        if (this.category == null) {
+            return false;
+        }
+        return true;
+    }
 
-	public boolean isSearchAny() {
-		return this.isSearchByCategory() || isSearchByCountry() || isSearchByYear();
-	}
+    public boolean isSearchByYear() {
+        if (this.year == 0) {
+            return false;
+        }
+        return true;
+    }
 
-	public boolean isSearchByCategory() {
-		if (this.category == null) {
-			return false;
-		}
-		return true;
-	}
+    public boolean isSearchByCountry() {
+        if (this.country == null) {
+            return false;
+        }
+        return true;
+    }
 
-	public boolean isSearchByYear() {
-		if (this.year == 0) {
-			return false;
-		}
-		return true;
-	}
-
-	public boolean isSearchByCountry() {
-		if (this.countryId == 13) {
-			return false;
-		}
-		return true;
-	}
-
-	public boolean isSearchByOrder() {
-		if (this.orderBy == 0) {
-			return false;
-		}
-		return true;
-	}
+    public boolean isSearchByOrder() {
+        if (this.orderBy == 0) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/main/java/com/vod/service/impl/MovieServiceImpl.java
+++ src/main/java/com/vod/service/impl/MovieServiceImpl.java
@@ -8,60 +8,62 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.vod.dao.MovieDao;
 import com.vod.model.Category;
+import com.vod.model.Country;
 import com.vod.model.Movie;
 import com.vod.service.MovieService;
 
 @Service
 @Transactional
 public class MovieServiceImpl implements MovieService {
-	@Autowired
-	private MovieDao dao;
+    @Autowired
+    private MovieDao dao;
 
-	@Override
-	public boolean add(Movie movie) {
-		return dao.add(movie);
-	}
+    @Override
+    public boolean add(Movie movie) {
+        return dao.add(movie);
+    }
 
-	@Override
-	public boolean update(Movie movie) {
-		return dao.update(movie);
-	}
+    @Override
+    public boolean update(Movie movie) {
+        return dao.update(movie);
+    }
 
-	@Override
-	public Movie get(Integer id) {
-		return dao.get(id);
-	}
+    @Override
+    public Movie get(Integer id) {
+        return dao.get(id);
+    }
 
-	@Override
-	public List<Movie> getAll() {
-		return dao.getAll();
-	}
+    @Override
+    public List<Movie> getAll() {
+        return dao.getAll();
+    }
 
-	@Override
-	public List<Movie> getByRate() {
-		return dao.getByRate();
-	}
+    @Override
+    public List<Movie> getByRate() {
+        return dao.getByRate();
+    }
 
-	@Override
-	public List<Movie> getByView() {
-		return dao.getByView();
-	}
+    @Override
+    public List<Movie> getByView() {
+        return dao.getByView();
+    }
 
-	@Override
-	public List<Movie> getByYear(Integer year) {
-		return dao.getByYear(year);
-	}
+    @Override
+    public List<Movie> getByYear(Integer year) {
+        return dao.getByYear(year);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.vod.service.MovieService#filterBy(java.lang.Integer,
-	 * java.lang.Integer, java.lang.Integer, java.lang.Integer,
-	 * java.lang.Integer)
-	 */
-	@Override
-	public List<Movie> filterBy(Integer orderId, Category category, Integer year, Integer countryId, Integer page) {
-		return dao.filterBy(orderId, category, year, countryId, page);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.vod.service.MovieService#filterBy(java.lang.Integer,
+     * java.lang.Integer, java.lang.Integer, java.lang.Integer,
+     * java.lang.Integer)
+     */
+    @Override
+    public List<Movie> filterBy(Integer orderId, Category category,
+            Integer year, Country country, Integer page) {
+        return dao.filterBy(orderId, category, year, country, page);
+    }
 
 }
